9da4a13e2cd5e0bcbf1fd5a0fab62af8dc569291
gennadas <gennadas_1@proton.me>
2025-01-21 21:13:54 +0000

    +

--- configuration.nix
+++ configuration.nix
@@ -137,6 +137,7 @@
     curl
     ipset
     iptables
+    amnezia-vpn
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -164,6 +165,6 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "24.11"; # Did you read the comment?
 
 }
