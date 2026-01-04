693d358d850de4ce67eba99d9726e444c33dc1a9
g <gennadas_1@proton.me>
2026-01-04 07:03:27 +0000

    attempting modularity2

--- flake.nix
+++ flake.nix
@@ -8,35 +8,46 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ... }: {
-    nixosConfigurations = {
-      surface = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { 
-          inherit nixpkgs-stable;
-          inherit (self) inputs;
+  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs-unstable = nixpkgs.legacyPackages.${system};
+      pkgs-stable = nixpkgs-stable.legacyPackages.${system};
+    in {
+      nixosConfigurations = {
+        surface = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = { 
+            inherit pkgs-stable;
+            inherit (self) inputs;
+          };
+          modules = [
+            ./hardware-configuration.nix
+            ./configuration.nix
+            ./modules/hardware/surface.nix
+            ./modules/desktop/hyprland.nix
+            ./modules/desktop/wayland.nix
+            ./modules/services/virtualization.nix
+            ./modules/services/audio.nix
+            ./modules/services/zapret.nix
+            ./modules/users/g.nix
+            
+            # Add module to inject pkgs-stable into module args
+            ({ pkgs, ... }: {
+              _module.args.pkgs-stable = pkgs-stable;
+            })
+            
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useUserPackages = true;
+                useGlobalPkgs = true;
+                extraSpecialArgs = { inherit pkgs-stable; };
+                users.g = import ./home.nix;
+              };
+            }
+          ];
         };
-        modules = [
-          ./hardware-configuration.nix
-          ./configuration.nix
-          ./modules/hardware/surface.nix
-          ./modules/desktop/hyprland.nix
-          ./modules/desktop/wayland.nix
-          ./modules/services/virtualization.nix
-          ./modules/services/audio.nix
-          ./modules/services/zapret.nix
-          ./modules/users/g.nix
-          
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useUserPackages = true;
-              useGlobalPkgs = true;
-              users.g = import ./home.nix;
-            };
-          }
-        ];
       };
     };
-  };
 }
